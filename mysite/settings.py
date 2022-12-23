8faa36ae144605505fba25ecdb108bd13673d147
ttuma <tumankov.tim@gmail.com>
2022-12-23 18:02:05 +1000

    Changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -130,3 +130,4 @@ MEDIA_URL = '/media/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
